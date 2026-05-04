0eb1015dea72e6500d4f8b75c53d301bc67a23f5
Devon Anderson <devonanderson5@gmail.com>
2026-05-03 16:11:03 -1000

    Expose strokeWidth prop for HandDrawnArrow and increase to 12

--- src/components/ui/HandDrawnArrow.tsx
+++ src/components/ui/HandDrawnArrow.tsx
@@ -1,4 +1,10 @@
-const HandDrawnArrow = ({ className = '' }: { className?: string }) => {
+const HandDrawnArrow = ({
+  className = '',
+  strokeWidth = 10,
+}: {
+  className?: string
+  strokeWidth?: number
+}) => {
   return (
     <svg
       width="237"
@@ -11,7 +17,7 @@ const HandDrawnArrow = ({ className = '' }: { className?: string }) => {
       <path
         d="M6.99659 70.8319C50.2561 95.8302 124.915 82.3674 144.053 48.1745C163.192 13.9815 121.999 -4.39624 104.915 40.658C87.8304 85.7122 130.35 90.6719 218.119 66.4182M218.119 66.4182L203.68 54.4172M218.119 66.4182L202.449 86.0201"
         stroke="currentColor"
-        strokeWidth="8"
+        strokeWidth={strokeWidth}
         strokeLinecap="round"
       />
     </svg>
