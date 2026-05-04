3892ec5b6d5a3fadeefb6961a026d3aa343e393f
Devon Anderson <devonanderson5@gmail.com>
2026-05-03 16:13:50 -1000

    Adjust horizontal margins to visually center the hero arrow

--- src/components/ui/HandDrawnArrow.tsx
+++ src/components/ui/HandDrawnArrow.tsx
@@ -12,7 +12,7 @@ const HandDrawnArrow = ({
       viewBox="0 0 237 106"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={`inline-block align-middle h-[0.8em] w-auto mx-[0.2em] relative top-[-0.1em] ${className}`}
+      className={`inline-block align-middle h-[0.8em] w-auto ml-[0.4em] mr-[0.2em] relative top-[-0.1em] ${className}`}
     >
       <path
         d="M6.99659 70.8319C50.2561 95.8302 124.915 82.3674 144.053 48.1745C163.192 13.9815 121.999 -4.39624 104.915 40.658C87.8304 85.7122 130.35 90.6719 218.119 66.4182M218.119 66.4182L203.68 54.4172M218.119 66.4182L202.449 86.0201"
