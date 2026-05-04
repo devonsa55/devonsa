6a28fe2510688b9af8f3f124714c8cf86e247200
Devon Anderson <devonanderson5@gmail.com>
2026-05-03 18:38:15 -1000

    Update hero arrow SVG to the latest version

--- src/components/ui/HandDrawnArrow.tsx
+++ src/components/ui/HandDrawnArrow.tsx
@@ -8,14 +8,14 @@ const HandDrawnArrow = ({
   return (
     <svg
       width="237"
-      height="106"
-      viewBox="0 0 237 106"
+      height="72"
+      viewBox="0 0 237 72"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={`inline-block align-middle h-[0.8em] w-auto ml-[0.2em] mr-[0.1em] relative top-[-0.1em] ${className}`}
+      className={`inline-block align-middle h-[0.6em] w-auto ml-[0.2em] mr-[0.1em] relative top-[-0.05em] ${className}`}
     >
       <path
-        d="M6.99659 70.8319C50.2561 95.8302 124.915 82.3674 144.053 48.1745C163.192 13.9815 121.999 -4.39624 104.915 40.658C87.8304 85.7122 130.35 90.6719 218.119 66.4182M218.119 66.4182L203.68 54.4172M218.119 66.4182L202.449 86.0201"
+        d="M8.32264 43.8319C51.5821 68.8302 130.951 58.1649 144.379 34.1745C157.807 10.184 118.448 -8.17056 105.241 26.658C84.1942 82.1613 131.676 63.6719 219.445 39.4182M219.445 39.4182L205.006 27.4172M219.445 39.4182L203.775 59.0201"
         stroke="currentColor"
         strokeWidth={strokeWidth}
         strokeLinecap="round"
